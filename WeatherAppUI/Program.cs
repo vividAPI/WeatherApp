d4c3d3e64703dbb3abe86ccbdf719d7fb72294f8
Alex Calder <acalder@vividapi.com>
2022-01-30 17:14:16 -0500

    Put in Do While and Try Catch

--- WeatherAppUI/Program.cs
+++ WeatherAppUI/Program.cs
@@ -2,22 +2,44 @@
 using System.Text.Json;
 
 
+bool more = true;
+do
+{
+    try
+    {
 
-Console.Write("Hello, what city are you interested in? ");
+        Console.Write("Hello, what city are you interested in? ");
 
 
-string? city = Console.ReadLine();
-string key = "feb5ef54e7831316166cf5ab9116a006";
+        string? city = Console.ReadLine();
+        string key = "feb5ef54e7831316166cf5ab9116a006";
 
-var url = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}");
+        var url = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}");
 
-HttpClient client = new HttpClient();
-client.BaseAddress = url;
+        HttpClient client = new HttpClient();
+        client.BaseAddress = url;
 
-string foo = await client.GetStringAsync(url);
+        string foo = await client.GetStringAsync(url);
 
-WeatherModel.Root jsonString = JsonSerializer.Deserialize<WeatherModel.Root>(foo);
+        WeatherModel.Root jsonString = JsonSerializer.Deserialize<WeatherModel.Root>(foo);
 
-Console.WriteLine($"The weather in {city} feels like {jsonString.main.feels_like} degrees F");
+        Console.WriteLine($"The weather in {city} feels like {jsonString.main.feels_like} degrees F");
+        Console.Write("Would you like to see another city? Y/N: ");
+        string? response = Console.ReadLine();
+        if (response.ToLower() == "y")
+        {
+            more = true;
+        }
+        else
+        {
+            more = false;
+        }
 
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine("You did something weird, not sure what? Maybe try another city?");
+    }
+} while (more);
 
